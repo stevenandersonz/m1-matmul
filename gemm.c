7670827935ad107392730c26a1bc3995195af27a
Steven Anderson <sdandersonz@icloud.com>
2023-09-22 13:00:59 -0400

    gflop

--- gemm.c
+++ gemm.c
@@ -8,8 +8,9 @@
 
 #define TOLERANCE 1e-3 
 #define N 1024
-#define BLOCK_SIZE_X 4 
-#define BLOCK_SIZE_Y 2 
+#define BLOCK_SIZE_X 8
+#define BLOCK_SIZE_K 8 
+#define BLOCK_SIZE_Y 8 
 
 float A[N][N] __attribute__ ((aligned(32)));
 float B[N][N] __attribute__ ((aligned(32)));
@@ -60,32 +61,33 @@ void simple_matmul_neon(float *A, float *B, float *C) {
 void matmul_neon(float *A, float *B, float *C) {
     for(int by = 0; by < N; by+=BLOCK_SIZE_Y) {
         for(int bx = 0; bx < N; bx+=BLOCK_SIZE_X) {
-            for(int y = by; y < by+BLOCK_SIZE_Y; y++) {
-                for(int x = bx; x < bx+BLOCK_SIZE_X; x+=4) {
-                    float32x4_t sum_vec0 = vdupq_n_f32(0.0f); // Initialize to zero
-                    float32x4_t sum_vec1 = vdupq_n_f32(0.0f); // Initialize to zero
-                    float32x4_t sum_vec2 = vdupq_n_f32(0.0f); // Initialize to zero
-                    float32x4_t sum_vec3 = vdupq_n_f32(0.0f); // Initialize to zero
-                    for(int k = 0; k < N;  k+=4) {
-                        float32x4_t a_vec = vld1q_f32(&A[y*N+k]);
-                        //float32x4_t b_vec = vld1q_f32(&B[k*N+x]);
-                        float32x4_t b_vec0 = {B[k * N + x], B[(k + 1) * N + x], B[(k + 2) * N + x], B[(k + 3) * N + x]};
-                        float32x4_t b_vec1 = {B[k * N + x + 1], B[(k + 1) * N + x + 1], B[(k + 2) * N + x + 1], B[(k + 3) * N + x + 1]};
-                        float32x4_t b_vec2 = {B[k * N + x + 2], B[(k + 1) * N + x + 2], B[(k + 2) * N + x + 2], B[(k + 3) * N + x + 2]};
-                        float32x4_t b_vec3 = {B[k * N + x + 3], B[(k + 1) * N + x + 3], B[(k + 2) * N + x + 3], B[(k + 3) * N + x + 3]};
-                        sum_vec0 = vmlaq_f32(sum_vec0, a_vec, b_vec0);
-                        sum_vec1 = vmlaq_f32(sum_vec1, a_vec, b_vec1);
-                        sum_vec2 = vmlaq_f32(sum_vec2, a_vec, b_vec2);
-                        sum_vec3 = vmlaq_f32(sum_vec3, a_vec, b_vec3);
+            for(int bk = 0; bk < N; bk+=BLOCK_SIZE_K) {
+                for(int y = by; y < by+BLOCK_SIZE_Y; y++) {
+                    for(int x = bx; x < bx+BLOCK_SIZE_X; x+=4) {
+                        float32x4_t sum_vec0 = vdupq_n_f32(0.0f);
+                        float32x4_t sum_vec1 = vdupq_n_f32(0.0f); 
+                        float32x4_t sum_vec2 = vdupq_n_f32(0.0f);
+                        float32x4_t sum_vec3 = vdupq_n_f32(0.0f);
+                        for(int k = bk; k < bk+BLOCK_SIZE_Y; k+=4) {
+                            float32x4_t a_vec = vld1q_f32(&A[y*N+k]);
+                            //float32x4_t b_vec = vld1q_f32(&B[k*N+x]);
+                            float32x4_t b_vec0 = {B[k * N + x], B[(k + 1) * N + x], B[(k + 2) * N + x], B[(k + 3) * N + x]};
+                            float32x4_t b_vec1 = {B[k * N + x + 1], B[(k + 1) * N + x + 1], B[(k + 2) * N + x + 1], B[(k + 3) * N + x + 1]};
+                            float32x4_t b_vec2 = {B[k * N + x + 2], B[(k + 1) * N + x + 2], B[(k + 2) * N + x + 2], B[(k + 3) * N + x + 2]};
+                            float32x4_t b_vec3 = {B[k * N + x + 3], B[(k + 1) * N + x + 3], B[(k + 2) * N + x + 3], B[(k + 3) * N + x + 3]};
+                            sum_vec0 = vmlaq_f32(sum_vec0, a_vec, b_vec0);
+                            sum_vec1 = vmlaq_f32(sum_vec1, a_vec, b_vec1);
+                            sum_vec2 = vmlaq_f32(sum_vec2, a_vec, b_vec2);
+                            sum_vec3 = vmlaq_f32(sum_vec3, a_vec, b_vec3);
+                        }
+                        // float sum_array[4];
+                        C[y*N+x] += vaddvq_f32(sum_vec0);
+                        C[y*N+x+1] += vaddvq_f32(sum_vec1);
+                        C[y*N+x+2] += vaddvq_f32(sum_vec2);
+                        C[y*N+x+3] += vaddvq_f32(sum_vec3);
                     }
-                    // float sum_array[4];
-                    C[y*N+x] += vaddvq_f32(sum_vec0);
-                    C[y*N+x+1] += vaddvq_f32(sum_vec1);
-                    C[y*N+x+2] += vaddvq_f32(sum_vec2);
-                    C[y*N+x+3] += vaddvq_f32(sum_vec3);
                 }
-            }
-            
+            } 
         }
     }
 }
