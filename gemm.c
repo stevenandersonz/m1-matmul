5f605862dbda7c8aca4b1bd173422b2a1ca05d0d
Steven Anderson <sdandersonz@icloud.com>
2023-09-24 17:18:04 -0400

    22 flops

--- gemm.c
+++ gemm.c
@@ -61,33 +61,35 @@ void simple_matmul_neon(float *A, float *B, float *C) {
 void matmul_neon(float *A, float *B, float *C) {
     for(int by = 0; by < N; by+=BLOCK_SIZE_Y) {
         for(int bx = 0; bx < N; bx+=BLOCK_SIZE_X) {
-            for(int bk = 0; bk < N; bk+=BLOCK_SIZE_K) {
-                for(int y = by; y < by+BLOCK_SIZE_Y; y++) {
-                    for(int x = bx; x < bx+BLOCK_SIZE_X; x+=4) {
+            float32x4_t acc[BLOCK_SIZE_Y][BLOCK_SIZE_X] = {};
+            for(int bk = 0; bk < N; bk++) {
+                for(int y = by; y < by; y++) {
+                    for(int x = bx; x < bx; x++) {
+                        //float32x4_t ta = vld1q_dup_f32(&A[(y+iy)*N + k]);
                         float32x4_t sum_vec0 = vdupq_n_f32(0.0f);
-                        float32x4_t sum_vec1 = vdupq_n_f32(0.0f); 
-                        float32x4_t sum_vec2 = vdupq_n_f32(0.0f);
-                        float32x4_t sum_vec3 = vdupq_n_f32(0.0f);
-                        for(int k = bk; k < bk+BLOCK_SIZE_Y; k+=4) {
+                         for(int k = bk; k < bk+BLOCK_SIZE_Y; k+=4) {
                             float32x4_t a_vec = vld1q_f32(&A[y*N+k]);
-                            //float32x4_t b_vec = vld1q_f32(&B[k*N+x]);
+                            //float32x4_t a_vec = vld1q_dup_f32(&A[by+y*N+k]);
                             float32x4_t b_vec0 = {B[k * N + x], B[(k + 1) * N + x], B[(k + 2) * N + x], B[(k + 3) * N + x]};
-                            float32x4_t b_vec1 = {B[k * N + x + 1], B[(k + 1) * N + x + 1], B[(k + 2) * N + x + 1], B[(k + 3) * N + x + 1]};
-                            float32x4_t b_vec2 = {B[k * N + x + 2], B[(k + 1) * N + x + 2], B[(k + 2) * N + x + 2], B[(k + 3) * N + x + 2]};
-                            float32x4_t b_vec3 = {B[k * N + x + 3], B[(k + 1) * N + x + 3], B[(k + 2) * N + x + 3], B[(k + 3) * N + x + 3]};
                             sum_vec0 = vmlaq_f32(sum_vec0, a_vec, b_vec0);
-                            sum_vec1 = vmlaq_f32(sum_vec1, a_vec, b_vec1);
-                            sum_vec2 = vmlaq_f32(sum_vec2, a_vec, b_vec2);
-                            sum_vec3 = vmlaq_f32(sum_vec3, a_vec, b_vec3);
                         }
-                        // float sum_array[4];
                         C[y*N+x] += vaddvq_f32(sum_vec0);
-                        C[y*N+x+1] += vaddvq_f32(sum_vec1);
-                        C[y*N+x+2] += vaddvq_f32(sum_vec2);
-                        C[y*N+x+3] += vaddvq_f32(sum_vec3);
                     }
                 }
             } 
+        }
+    }
+}
+void matmul(float* const A, float* const B, float* const C){
+    for (int y=0; y<N; y++){
+        for (int k=0; k<N; k++){
+            float32x4_t a_vec = vdupq_n_f32( A[y * N + k]);
+            for (int x=0; x<N; x+=4){
+                float32x4_t sum_vec = vld1q_f32(&C[y * N + x]);
+                float32x4_t b_vec = vld1q_f32(&B[k*N+x]);
+                sum_vec = vmlaq_f32(sum_vec, a_vec, b_vec);
+                vst1q_f32(&C[y * N + x], sum_vec);
+            }
         }
     }
 }
@@ -97,29 +99,14 @@ int main (){
     // init arrays
     load_m("A.txt", A);
     load_m("B.txt", B);
-    uint64_t start = nanos();
-
-    // compute
-    // for (int by=0; by<N; by+=BLOCK_SIZE){
-    //     for (int bx=0; bx<N; bx+=BLOCK_SIZE){
-    //         for (int k=0; k<N; k+=BLOCK_SIZE){
-    //             for(int y = by; y < by + BLOCK_SIZE; y++) {
-    //                 for(int x = bx; x < bx + BLOCK_SIZE; x++) {
-    //                     for(int k1 = k; k1 < k + BLOCK_SIZE; k1++) {
-    //                         C[y][x] += A[y][k1] * B[k1][x];
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
-    matmul_neon(&A[0][0], &B[0][0], &C[0][0]);
-    uint64_t end = nanos();
-    double tflop = (2.0*N*N*N) * 1e-9;
-    double s = (end - start) * 1e-9;
-    printf("C's %f GFLOP/S \n", tflop/s);
-
-    // verify
+        uint64_t start = nanos();
+        matmul(&A[0][0],&B[0][0],&C[0][0]);
+    //  matmul_neon(&A[0][0], &B[0][0], &C[0][0]);
+        uint64_t end = nanos();
+        double tflop = (2.0*N*N*N) * 1e-9;
+        double s = (end - start) * 1e-9;
+        printf("C's %f GFLOP/S \n", tflop/s);
+   // verify
     load_m("C.txt", D);
     for (int i=0; i<N; i++){
         for (int j=0; j<N; j++){
