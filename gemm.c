c64d6b48619d9cb30b8d85fad8d4c01237e718db
Steven Anderson <sdandersonz@icloud.com>
2023-09-29 23:41:52 -0400

    58gflops

--- gemm.c
+++ gemm.c
@@ -6,17 +6,21 @@
 #include <string.h> 
 #include <arm_neon.h>
 
+// using accelerate give me ~700 gflop/s 
+//clang gemm.c -o gemm.out -framework Accelerate && ./gemm.out
+//#include <Accelerate/Accelerate.h>
+
 #define TOLERANCE 1e-3 
 #define N 1024
-#define BLOCK_X 16
+#define BLOCK_X 16 
 #define BLOCK_K 8 
 #define BLOCK_Y 8 
 #define BLOCK 8 
 
-float A[N*N] __attribute__ ((aligned(16)));
-float B[N*N] __attribute__ ((aligned(16)));
-float C[N*N] __attribute__ ((aligned(16)));
-float D[N*N] __attribute__ ((aligned(16)));
+float A[N*N] __attribute__ ((aligned(64)));
+float B[N*N] __attribute__ ((aligned(64)));
+float C[N*N] __attribute__ ((aligned(64)));
+float D[N*N] __attribute__ ((aligned(64)));
 uint64_t nanos(){
     struct timespec start;
     clock_gettime(CLOCK_MONOTONIC_RAW, &start);
@@ -36,6 +40,25 @@ void load_m (const char *nm, float* const M){
 
     fclose(fp);
 }
+
+// void matrix_multiply(const float* A, const float* B, float* C) {
+//     cblas_sgemm(
+//         CblasRowMajor,    // Order
+//         CblasNoTrans,     // Transpose for A
+//         CblasNoTrans,     // Transpose for B
+//         N, N, N,          // Dimensions of the matrices
+//         1.0f,             // Alpha
+//         A, N,             // Matrix A and its leading dimension
+//         B, N,             // Matrix B and its leading dimension
+//         0.0f,             // Beta
+//         C, N              // Matrix C and its leading dimension
+//     );
+// }
+
+// M1 chip:
+// rate	3.2 GHz
+// l1 cache 192kb+128kb
+// fmas? 
 void matmul(float* const A, float* const B, float* const C){
     for(int by = 0; by < N; by+=BLOCK_Y) {
         for(int bx = 0; bx < N; bx+=BLOCK_X) {
@@ -43,6 +66,7 @@ void matmul(float* const A, float* const B, float* const C){
                 for (int y=by; y<by+BLOCK_Y; y++){
                     for (int x=bx; x<bx+BLOCK_X; x+=4){
                         float32x4_t sum_vec = vld1q_f32(&C[y * N + x]);
+                        __builtin_prefetch(&C[y * N + x + 4], 1, 0);
                         for (int k=bk; k<bk+BLOCK_K; k++){
                             float32x4_t a_vec = vdupq_n_f32(A[y * N + k]);
                             float32x4_t b_vec = vld1q_f32(&B[k*N+x]);
@@ -55,26 +79,7 @@ void matmul(float* const A, float* const B, float* const C){
         }
     }
 }
-// for (int y = sy; y < ey; y+=BLOCK_Y) {
-//     for (int x = 0; x < N; x+=BLOCK*BLOCK_X) {
 
-//       __m256 acc[BLOCK_Y][BLOCK_X] = {};
-//       for (int k = 0; k < N; k++) {
-//         for (int iy = 0; iy < BLOCK_Y; iy++) {
-//           __m256 ta = _mm256_broadcast_ss(&A[(y+iy)*N + k]);
-//           for (int ix = 0; ix < BLOCK_X; ix++) {
-//             acc[iy][ix] = _mm256_fmadd_ps(ta, Bfm[((x+ix*BLOCK)*N + k*8)/8], acc[iy][ix]);
-//           }
-//         }
-//       }
-
-//       for (int iy = 0; iy < BLOCK_Y; iy++) {
-//         for (int ix = 0; ix < BLOCK_X; ix++) {
-//           Cm[((y+iy)*N + x + ix * BLOCK)/8] = acc[iy][ix];
-//         }
-//       }
-//     }
-//   }
 int main (){
     //generate arrays and compute numpy time
     system("python3 gemm.py");
@@ -83,7 +88,7 @@ int main (){
     load_m("B.txt", B);
         uint64_t start = nanos();
         matmul(A,B,C);
-    //  matmul_neon(&A[0][0], &B[0][0], &C[0][0]);
+        //matrix_multiply(A,B,C);
         uint64_t end = nanos();
         double tflop = (2.0*N*N*N) * 1e-9;
         double s = (end - start) * 1e-9;
